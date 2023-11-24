d87c9a31a94550082e8f2d5104b28326f905f0b4
Johan Walles <johan.walles@gmail.com>
2023-11-24 06:58:30 +0100

    Fix some names and descriptions

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Timed Times Tables', // FIXME: Get this from some Metadata file
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Timed Times Tables'),
     );
   }
 }
