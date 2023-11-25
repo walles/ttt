7da11d16bf8bf079024fe6e0e51b7223d536eda3
Johan Walles <johan.walles@gmail.com>
2023-11-25 14:49:50 +0100

    Remove dead code

--- lib/main.dart
+++ lib/main.dart
@@ -55,19 +55,7 @@ class TttHomeScreen extends StatefulWidget {
 }
 
 class _TttHomeScreenState extends State<TttHomeScreen> {
-  int _counter = 0;
   bool _running = false;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   void _toggleRunning() {
     setState(() {
@@ -116,11 +104,6 @@ class _TttHomeScreenState extends State<TttHomeScreen> {
       body: Center(
         child: child,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
