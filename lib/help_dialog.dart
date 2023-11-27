0eefdf8964ea2baab648488e29cbcb5070f2f033
Johan Walles <johan.walles@gmail.com>
2023-11-27 15:14:52 +0100

    Swedishify the help dialog

--- lib/help_dialog.dart
+++ lib/help_dialog.dart
@@ -2,6 +2,8 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 Widget bulletTextUrl(BuildContext context, String text, Uri? uri) {
   if (uri == null) {
     return SelectableText.rich(TextSpan(text: "\u2022 $text"));
@@ -29,22 +31,25 @@ Widget bulletTextUrl(BuildContext context, String text, Uri? uri) {
 void showHelpDialog(BuildContext context) {
   // With clickable links to the source code, GitHub Issues and for e-mailing
   // Johan
+  var l10n = AppLocalizations.of(context)!;
+
   showDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        title: const Text("Help"),
+        title: Text(l10n.help),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("A game to help you learn times tables. "),
+            Text(l10n.a_game_to_help_you_learn_times_tables),
             const SizedBox(height: 16),
-            bulletTextUrl(context, "Source code: ",
+            bulletTextUrl(context, "${l10n.source_code}: ",
                 Uri.parse("https://github.com/walles/ttt")),
-            bulletTextUrl(context, "Issues: ",
+            bulletTextUrl(context, "${l10n.issues}: ",
                 Uri.parse("https://github.com/walles/ttt/issues")),
-            bulletTextUrl(context, "Contact: johan.walles@gmail.com", null),
+            bulletTextUrl(
+                context, "${l10n.contact}: johan.walles@gmail.com", null),
           ],
         ),
         actions: [
