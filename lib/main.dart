311b2a6333b368cd835bf0aba312964e7b704619
Johan Walles <johan.walles@gmail.com>
2023-11-27 13:50:35 +0100

    Start following I18N instructions
    From here:
    https://docs.flutter.dev/ui/accessibility-and-internationalization/internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:ttt/config.dart';
 import 'package:ttt/game.dart';
 import 'package:ttt/help_dialog.dart';
@@ -17,26 +18,24 @@ class TttApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Timed Times Tables', // FIXME: Get this from some metadata file
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+
+        // FIXME: We get a red warning message in the console if we don't have
+        // this, but since we don't use any other Cupertino widgets, I think we
+        // should be fine without it.
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('sv'),
+      ],
+      home: const TttHomeScreen(title: 'Timed Times Tables'),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TttHomeScreen(title: 'Timed Times Tables'),
     );
   }
 }
